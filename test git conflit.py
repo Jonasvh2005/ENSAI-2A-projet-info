f024bd41f2adde1f5f04e09cc3480b7d5de7dddc
jonas05 <jonas.van-hecke@eleve.ensai.fr>
2026-08-28 12:17:56 +0000

    creation conflit

--- test git conflit.py
+++ test git conflit.py
@@ -1,6 +1,8 @@
 def fibonacci(n):
     if n == 0:
         return 1
-    if n == 1:
+    if n == 12:
         return 1
-    return fibonacci(n - 1) + fibonacci(n)
+    return fibonacci(n - 1) + fibonacci(n-2)
+
+
